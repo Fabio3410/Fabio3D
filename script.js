4bba06f6bd15c98da9afb9d8afbd3971e7e70f76
Fabio3410 <faselfabio@gmail.com>
2026-06-30 20:21:00 +0200

    Update script.js
    Added smarter Sliders for easier use

--- script.js
+++ script.js
@@ -19,20 +19,46 @@
             }
             window.scrollTo(0, 0);
         }
-let currentSlide = 0;
 
-function changeSlide(direction) {
-  const slidesContainer = document.querySelector('.slides');
-  const totalSlides = document.querySelectorAll('.slides img').length;
-  currentSlide += direction;
-  if (currentSlide >= totalSlides) {
-    currentSlide = 0;
-  } 
-  else if (currentSlide < 0) {
-    currentSlide = totalSlides - 1;
-  }
-  slidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
-}
+        let currentSlide = 0;
+        function changeSlide(direction) {
+                const slidesContainer = document.querySelector('.slides');
+                const totalSlides = document.querySelectorAll('.slides img').length;
+                currentSlide += direction;
+                if (currentSlide >= totalSlides) {
+                        currentSlide = 0;
+                } 
+                else if (currentSlide < 0) {
+                        currentSlide = totalSlides - 1;
+                }
+                slidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+        }
+
+        const sliderIndexes = new Map();
+        function changeSlide(buttonElement, direction) {
+                const container = buttonElement.closest('.slider-container');
+                if (!container) return;
+                if (!container.id) {
+                        container.id = 'slider-' + Math.random().toString(36).substr(2, 9);
+                }
+                
+        const sliderId = container.id;
+        if (!sliderIndexes.has(sliderId)) {
+        sliderIndexes.set(sliderId, 0);
+        }
+        let currentSlide = sliderIndexes.get(sliderId);
+                const slidesContainer = container.querySelector('.slides');
+                const totalSlides = container.querySelectorAll('.slides img').length;
+                currentSlide += direction;
+                if (currentSlide >= totalSlides) {
+                        currentSlide = 0;
+                } 
+                else if (currentSlide < 0) {
+                        currentSlide = totalSlides - 1;
+                }
+                sliderIndexes.set(sliderId, currentSlide);
+                slidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+        }
 
         let cart = [];
         function toggleCart() {
